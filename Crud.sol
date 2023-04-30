fb412157a68b6e59df89f46897bc71f4978bb984
Kevin Tamakuwala <kevintamakuwala16@gmail.com>
2023-04-30 13:09:34 +0530

    Create Crud.sol

---
+++ Crud.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.18;
+
+contract MyContract {
+    string value;
+
+    constructor() {
+        value = "myValue";
+    }
+
+    function get() public view returns (string memory) {
+        return value;
+    }
+
+    function set(string memory _value) public {
+        value = _value;
+    }
+}
